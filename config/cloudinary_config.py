7587fe5eb49fc093960f5968370e8ffdf7898a21
shyam <meghashyamreddy014@gmail.com>
2025-07-31 09:25:35 +0530

    Changes made

--- config/cloudinary_config.py
+++ config/cloudinary_config.py
@@ -7,9 +7,9 @@ load_dotenv()
 def configure_cloudinary():
     """Configure Cloudinary with credentials"""
     cloudinary.config(
-        cloud_name="dktvtxucf",
-        api_key="323414932774916",
-        api_secret="QXMWwaknKWEjJ5hIAC6xKSxeq08",
+        cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME", "dktvtxucf"),
+        api_key=os.getenv("CLOUDINARY_API_KEY", "323414932774916"),
+        api_secret=os.getenv("CLOUDINARY_API_SECRET", "QXMWwaknKWEjJ5hIAC6xKSxeq08"),
         secure=True
     )
 
